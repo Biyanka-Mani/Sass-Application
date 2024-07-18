84aab233e3bc7c2b17cc63c60dcaf359719353bd
Biyanka-Mani <biyanka167@gmail.com>
2024-07-18 11:27:48 +0530

    Mailling is set

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,17 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  # Set up mail delivery method and settings
+  config.action_mailer.delivery_method = :smtp
+      
+  config.action_mailer.smtp_settings = {
+                                        address: 'smtp.gmail.com',
+                                        port: 587,
+                                        domain: 'gmail.com',
+                                        user_name: 'biyanka167@gmail.com',
+                                        password: 'mlxoprfgridznmbi',
+                                        authentication: 'login',
+                                        enable_starttls_auto: true
+                                      }
 end
